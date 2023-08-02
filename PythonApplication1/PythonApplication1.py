66f81d9f2c2bc12a0b184480d9613145d3f2fed3
Chowdhury-Ashhabur <Ashhabur.Chowdhury.2018@live.rhul.ac.uk>
2023-08-02 20:45:13 +0100

    Fixed errors

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -5,8 +5,8 @@ CLIENT = RESTClient(api_key="n7T7pW1Ius5xnMnQmOe_37XNGLNWavdu")
 ticker = "AAPL"
 
 # start off by setting todays date as the start/end date.
-startDate = datetime.datetime.now()
-endDate = datetime.datetime.now()
+startDateInput = datetime.datetime.now()
+endDateInput = datetime.datetime.now()
 
 def formatInputDates(date):
     """
@@ -15,16 +15,24 @@ def formatInputDates(date):
     : param1 date: The start date or end date to format.
 
     """
-    return date.strftime("%d-%m-%Y")
+    try:
+        returnValue = datetime.datetime.strptime(date, "%d/%m/%Y")
+    except:
+        returnValue = ""
+
+    return returnValue
 
 def formatDates(date):
     """
-    format the date (YYYY/mm/dd), used to format in the way Polygon accepts.
+    format the date (YYYY-mm-dd), used to format in the way Polygon accepts.
 
     : param1 date: The start date or end date to format.
 
     """
-    return date.strftime("%Y-%m-%d")
+    returnValue = datetime.datetime.strptime(date, "%d/%m/%Y")
+    returnValue = returnValue.strftime("%Y-%m-%d")
+
+    return returnValue
 
 def getInputs():
     """
@@ -32,25 +40,34 @@ def getInputs():
     Then get the end date the same way and check again.
 
     """
+    global startDateInput
+    global endDateInput
+
     startDateInput = input("Enter the start date in dd/mm/yyyy format, or to exit input exit: ")
     if (startDateInput.upper() == "EXIT"):
         exit()
-    while (startDateInput == formatInputDates(startDateInput)):
-        startDateInput = input("Enter the start date in dd/mm/yyyy format, or to exit input exit: ")
+
+    while (formatInputDates(startDateInput) == ""):
+        startDateInput = input("Invalid input, enter the start date in dd/mm/yyyy format, or to exit input exit: ")
         if (startDateInput.upper() == "EXIT"):
             exit()
+
     endDateInput = input("Enter the end date in dd/mm/yyyy format, or to exit input exit: ")
     if (endDateInput.upper() == "EXIT"):
         exit()
-    while (endDateInput == formatInputDates(endDateInput)):
-        endDateInput = input("Enter the start date in dd/mm/yyyy format, or to exit input exit: ")
+
+    while (formatInputDates(endDateInput) == ""):
+        endDateInput = input("Invalid input, enter the end date in dd/mm/yyyy format, or to exit input exit: ")
         if (endDateInput.upper() == "EXIT"):
             exit()
 
 def getData():
+    global startDateInput
+    global endDateInput
+
     # List Aggregates (Bars)
     aggs = []
-    for a in CLIENT.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_=formatDates(startDate), to=formatDates(endDate), limit=50000):
+    for a in CLIENT.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_=formatDates(startDateInput), to=formatDates(endDateInput), limit=50000):
         aggs.append(a)
 
     print(aggs)
@@ -63,11 +80,18 @@ def main():
     Then gets the data.
 
     """
+    global startDateInput
+    global endDateInput
+
     getInputs()
-    userConfirmation = input("Do you want to get the data from " + startDate + " to " + endDate + " ? (Y/N) ")
-    while (userConfirmation != "Y" or userConfirmation != "N"):
-        userConfirmation = input("Input invalid, do you want to get the data from " + startDate + " to " + endDate + " ? (Y/N) ")
-    if (userConfirmation == "N"):
+    userConfirmation = input("Do you want to get the data from " + startDateInput + " to " + endDateInput + " ? (Y/N) ")
+    while (not(userConfirmation == 'Y' or userConfirmation == 'N')):
+        userConfirmation = input("Input invalid, do you want to get the data from " + startDateInput + " to " + endDateInput + " ? (Y/N) ")
+
+    if (userConfirmation == 'N'):
         getInputs()
     else:
         getData()
+
+
+main()
